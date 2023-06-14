66bd82b5dc3eafaebb562f24a203b5c16ed726d8
chebotarevdmitr <127180093+chebotarevdmitr@users.noreply.github.com>
2023-06-14 14:08:37 +0300

    factorial

---
+++ Begin_15..cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+	
+  printf("       The factorial of a number");	
+	//The factorial of a number 
+	// 6!=1*2*3*4*5*6=720
+   int number;
+   cout << "Enter number : ";	
+   cin >> number;	
+    
+   int factorial=1;
+   
+  /*  for(int i=1;i<=number; i++) {
+   	  factorial = factorial * i ;
+      }
+   	 cout << number << " != " << factorial;
+  */  
+  
+   // 6!=6*5*4*3*2*1=720
+   for(int i= number;i>=1;i-- ) {
+   	 factorial = factorial * i;	
+   }   	 
+   cout << number << " != " << factorial;
+	system("pause>0");
+}
